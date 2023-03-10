945e6c8c1729fb30e4fa542088147c22d7b4a722
alanbontoft <alan.bontoft@gmail.com>
2023-03-10 19:10:35 +0000

    Settings updated for picoprobe debugger

--- ProjectFiles/main.cpp
+++ ProjectFiles/main.cpp
@@ -10,12 +10,15 @@ MathClass *pMath;
 void led_task(void*)
 {   
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    TickType_t delay;
+    bool fast = false;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
     
     pMath = new MathClass(1);
 
+    /*
     while (true)
     {
         gpio_put(LED_PIN, 1);
@@ -26,6 +29,21 @@ void led_task(void*)
         pMath->inc();
 
         if (pMath->value() == 11) pMath->setValue(1);
+    }
+    */
+
+    while (true)
+    {
+        delay = fast ? 10 : 50;
+        for (int i=0; i < 10; i++)
+        {
+            gpio_put(LED_PIN, 1);
+            vTaskDelay(delay);
+            gpio_put(LED_PIN, 0);
+            vTaskDelay(delay);
+        }
+
+        fast = !fast;
     }
 }
 
